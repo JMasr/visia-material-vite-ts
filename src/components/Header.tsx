d5100fb09094ebb76b8ab635ac6b299b65627a2f
José M. Ramírez <32576858+JMasr@users.noreply.github.com>
2023-10-02 18:49:20 +0200

    Set the color to the Visia-Form app

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import { colors } from "@mui/material";
 
 const Header: React.FC = () => {
   return (
     <AppBar position="static">
-      <Toolbar>
+      <Toolbar style={{ backgroundColor: "#2788ff" }}>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           Visia - Grabación de sesiones.
         </Typography>
