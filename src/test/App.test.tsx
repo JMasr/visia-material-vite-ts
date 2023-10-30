6df18e73fbea6c0284e35966f30c7839b1a96e54
José M. Ramírez <moscaelectronica@gmail.com>
2023-10-30 16:13:51 +0100

    Refactoring the main test App

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,7 +1,10 @@
 import { expect, test } from "vitest";
-import App from "../App";
+import BackendHandler from "../api/backendHandler";
+import Record from "../screen/Record";
 
 test("Check component", () => {
-  const app = new App();
-  expect(app).not.toBe(null);
+  const baseUrl = "http://127.0.0.1:5000";
+  const backendHandler = new BackendHandler(baseUrl);
+  // Test backendHandler
+  expect(backendHandler).toBeDefined();
 });
