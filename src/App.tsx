41cf150cab77ec7f43044ac78fb99792223c186c
José M. Ramírez <moscaelectronica@gmail.com>
2023-11-10 17:25:31 +0100

    Moving the poll method of backend

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import "./App.css";
 import "@fontsource/roboto";
 import Record from "./screen/Record";
 import BackendHandler from "./api/backendHandler";
-import React from "react";
+import { useEffect } from "react";
 
 const baseUrl = "http://127.0.0.1:8181";
 const backendHandler = new BackendHandler(baseUrl);
 
 export default function App() {
+  useEffect(() => {
+    // Call the pollBackEnd method when the component mounts
+    backendHandler.pollBackEnd();
+  }, []);
+
   return <Record backendHandler={backendHandler} />;
 }
